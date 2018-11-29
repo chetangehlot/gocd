5e6ead9fb02ae27ec1ebfd1f90357ec3d52eaa19
chetangehlot <cpgehlot4@gmail.com>
2018-11-29 15:59:42 +0530

    solve file

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+apt-get update
+
+apt-get install -y curl sudo 
+
+curl -sSL http://get.docker.com | sh
+
 docker build -t nginx-test .
 
 docker run -d --name nginx-text -p 8500:80 nginx-test
